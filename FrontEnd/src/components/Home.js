4b64be61e65894c141a74b6c82a7a2f131d4ebfb
willop <wilfredp159@gmail.com>
2022-03-01 16:02:46 -0600

    Upddate frontend

--- FrontEnd/src/components/Home.js
+++ FrontEnd/src/components/Home.js
@@ -1,82 +1,36 @@
 import React, { useState } from 'react'
 import { Form, Button, Row, Col, Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
 import BarraNavegacion from '../components/BarraNavegacion'
+import Presentacion from '../components/Home/Presentacion'
 import '../Style/Home.css'
 
 
 
 export function Home (){
 
-    const[switchComp,setSwitch] = useState(false);
-    const [imagenmostrar,setimg]=useState("https://cdn.pixabay.com/photo/2015/02/09/20/03/koala-630117__340.jpg")
-    const [nombre,setnombre]=useState("Usuario Defaultt")
-    const [username,setusername]=useState("Username Defaultsss")
-    
+    //const[switchComp,setSwitch] = useState(false);
+    const[estadodiv, setEstadodiv] = useState(1)
 
-    function handleChange(evt) {
-        
-      }
 
-    const enviarDatos = async(event)=>{
-        console.log(imagenmostrar)
-        try {
-            let configuracion = {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(setimg)
-            }
-            let respuesta = await fetch('http://localhost:4500/home', configuracion)
-            let json = await respuesta.json();
-            console.log('valor de la respuesta json')
-            console.log(json)
-            setimg("data:image/png;base64, "+json.foto)
-            //setnombre(json.nombre) 
-            //setusername(json.username)
-        } catch (error) {
+    function Componente () {
+        if (estadodiv===1) {
+            //console.log(img.Nombre+""+img.Apellido+""+img.FechaNacimiento+""+img.CorreoElectronico+""+img.TipoMembresia+""+img.FotoUsuario)            
+            return <Presentacion/> // <FormularioPerfil name={img.Nombre} apellido={img.Apellido} Fecha={img.FechaNacimiento}  correo={img.CorreoElectronico} membresia={img.TipoMembresia}  imgg={img.FotoUsuario} />
+        } else if (estadodiv===2) {
+            //console.log("Accion en membresia")
+            return ""
+        } else if (estadodiv===3) {
+            //console.log("Accion en membresia")
+            return ""
         }
+            return ""
     }
 
-
-
   return (
-    <div id="id_bodyHome">
-                <BarraNavegacion />
-                <div id="id_foto">
-                    <img id="foto_id" src={imagenmostrar} />
-                </div>
-                <div id="id_informacion">
-                    <Form>
-                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-                            <Form.Label column sm="2">
-                                Nombre
-                            </Form.Label>
-                            <Col sm="10">
-                                <Form.Control plaintext readOnly value={nombre} onChange={handleChange} />
-                            </Col>
-                        </Form.Group>
-
-                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-                            <Form.Label column sm="2">
-                                Username
-                            </Form.Label>
-                            <Col sm="10">
-                                <Form.Control plaintext readOnly defaultValue={username} onChange={handleChange}/>
-                            </Col>
-                        </Form.Group>
-                    </Form>
-                </div>
-                <div id="id_opciones">
-                    <br/>
-                    <Button variant="Enviar prueba" onClick={enviarDatos}>Primary</Button>
-                    <br/>
-                    <Button variant="primary">Secondary</Button>
-                    <br/>
-                    <Button variant="primary">tercero</Button>
-                </div>
-            </div>
+    <div>
+        <BarraNavegacion/>
+        <Componente/>
+    </div>
   )
 }
 
---
+++ FrontEnd/src/components/Home/Presentacion.js
@@ -0,0 +1,74 @@
+import React,{ useState } from 'react'
+import { Form, Button, Row, Col, Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
+
+export default function Presentacion() {
+
+    const [imagenmostrar,setimg]=useState("https://cdn.pixabay.com/photo/2015/02/09/20/03/koala-630117__340.jpg")
+    const [nombre,setnombre]=useState("Usuario Defaultt")
+    const [username,setusername]=useState("Username Defaultsss")
+
+
+    const enviarDatos = async(event)=>{
+        console.log(imagenmostrar)
+        try {
+            let configuracion = {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(setimg)
+            }
+            let respuesta = await fetch('http://localhost:4500/home', configuracion)
+            let json = await respuesta.json();
+            console.log('valor de la respuesta json')
+            console.log(json)
+            setimg("data:image/png;base64, "+json.foto)
+            //setnombre(json.nombre) 
+            //setusername(json.username)
+        } catch (error) {
+        }
+    }
+
+    function cambio() {
+        setnombre("nuevo nombre")
+        setusername("nuevo username")
+    }
+
+  return (
+    <div id="id_bodyHome">
+                <div id="id_foto">
+                    <img id="foto_id" src={imagenmostrar} />
+                </div>
+                <div id="id_informacion">
+                    <Form>
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                            <Form.Label column sm="2">
+                                Nombre
+                            </Form.Label>
+                            <Col sm="10">
+                                <Form.Control plaintext readOnly value={nombre} />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                            <Form.Label column sm="2">
+                                Username
+                            </Form.Label>
+                            <Col sm="10">
+                                <Form.Control plaintext readOnly value={username} />
+                            </Col>
+                        </Form.Group>
+                    </Form>
+                </div>
+                <div id="id_opciones">
+                    <br/>
+                    <Button variant="primary" onClick={enviarDatos}>Ver Fotos</Button>
+                    <br/>
+                    <Button variant="primary" onClick={cambio}>Subir Foto</Button>
+                    <br/>
+                    <Button variant="primary">Editar Albumes</Button>
+                </div>
+            </div>
+  )
+}
